e50d9d7b315eccab9b9c18938af1bb110c144e99
NAKAK10 <10madhatter06@gmail.com>
2023-06-02 15:59:49 +0900

    add option

--- getFirestore/main.py
+++ getFirestore/main.py
@@ -8,11 +8,12 @@ db = firestore.client()  # firestore接続
 
 
 # 情報を取得
-def funGetFirestore(collection):
+def funGetFirestore(collection, is_have_id=False):
     docs = db.collection(collection).get()
-    d = []
+    d = {} if is_have_id else []
     for doc in docs:
-        d.append(doc.to_dict())
-    if d == []:
-        return []
+        if is_have_id:
+            d[doc.id] = doc.to_dict()
+        else:
+            d.append(doc.to_dict())
     return d
--- main.backup.py
+++ main.backup.py
@@ -1,14 +1,22 @@
+"""
+バックアップのみを行う
+"""
+
 import datetime
 from backup.file import funWriteFile
 from backup.date import funDateFormat
 from getFirestore.main import funGetFirestore
 
+"""
+if is_have_id is True, funGetFirestore return dict
+if is_have_id is False, funGetFirestore return list
+"""
+is_have_id = True
 
 backupCollection = ['AAAA']
 
-
 for collentionName in backupCollection:
-    getCollettion = funGetFirestore(collentionName)
+    getCollettion = funGetFirestore(collentionName, is_have_id)
     fileName = 'backupDate/' + funDateFormat(datetime.datetime.now(), 'YYYY年MM月DD日/') + collentionName + '.json'
     print(fileName)
     funWriteFile(getCollettion, fileName)
